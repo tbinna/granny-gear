f47f878288f87e89cf4e032100c3f46ed088268c
Tobias Binna <tobi.binna@gmail.com>
2014-11-26 01:55:08 +0800

    Added standard deviation to gear stats.

--- app/helpers/analysis_helper.rb
+++ app/helpers/analysis_helper.rb
@@ -1,2 +1,13 @@
 module AnalysisHelper
+	def mean(array)
+		array.inject(0) { |sum, x| sum += x } / array.size.to_f
+	end
+
+	def variance(array)
+		array.inject(0) { |variance, x| variance += (x - mean(array)) ** 2 }
+	end
+
+	def standard_deviation(array)
+		Math.sqrt(variance(array)/(array.size-1))
+	end
 end
